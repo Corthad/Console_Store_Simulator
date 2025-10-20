965e08458b30c92c80515abc9d3fa23a073ac4ba
DgTal8Solver <nik2284856@mail.ru>
2025-10-20 23:36:22 +0200

    feat: добавлено определение пути к рабочей папки для получения данных из data

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
 #include "storage.h"
 #include <iostream>
 #include <vector>
+#include <filesystem> // Для получения пути к рабочей папке
 
 int main()
 {
     setlocale(LC_ALL, "ru_RU.UTF-8"); // Установка кодировки UTF-8
 
-    std::vector<product> items = load_data("products.txt");
-    items_list(items); // Вывод данных в консоль
+    std::string current_path = std::filesystem::current_path().string();
+    std::string products_path = current_path + "/data/products.txt";
+    std::string updated_path = current_path + "/data/products_upd.txt";
 
-    add_item(items, 0, 5); // Увеличится количество на 5
-    remove_item(items, 1); // Уменьшится количество на 1
+    load_data(products_path);
+    show_storage(); // Вывод данных в консоль
 
-    set_item_price(items, 0, 777); // Устанавливается значение цены, равное 777
-    set_item_weight(items, 1, 100); // Устанавливается значение веса, равное 100
+    add_item(0, 5); // Увеличится количество на 5
+    remove_item(1); // Уменьшится количество на 1
 
-    items_list(items); // Вывод данных в консоль
+    set_item_price(0, 777); // Устанавливается значение цены, равное 777
+    set_item_weight(1, 100); // Устанавливается значение веса, равное 100
 
-    save_data("products_updated.txt", items); // Сохранение данных в файл 'products_updated.txt'
+    show_storage(); // Вывод данных в консоль
 
+    save_data(updated_path); // Сохранение данных в файл 'products_updated.txt'
+    
     system("pause");
     return 0;
 }
