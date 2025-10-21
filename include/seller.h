ff5bd742790afd021002d190d95cdcd76b030c42
Svinthefirst <kerineba@gmail.com>
2025-10-21 20:16:11 +0200

    feat: Заготовка логика обработки покупателя со стороны продавца

---
+++ include/seller.h
@@ -0,0 +1,4 @@
+#include <string>
+using namespace std;
+
+void serve_buyer(string product_name, int quantity);
---
+++ src/seller.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+#include "../include/storage.h"
+#include <map>
+
+using namespace std;
+
+bool served = false;
+
+void sell_products(string args)
+{
+	cout << "args: " << args << endl;
+}
+
+void add_product(string args)
+{
+	cout << "args: " << args << endl;
+}
+
+void show_products(string args)
+{
+	cout << "args: " << args << endl;
+}
+
+void ring_up_product(string args)
+{
+	cout << "args: " << args << endl;
+}
+
+map<string, void(*)(string)> commands { {"add", add_product}, { "sell", sell_products }, { "show", show_products }, { "ring", ring_up_product } };
+
+void serve_buyer(string product_name, int quantity)
+{
+	string input;
+	while (!served)
+	{
+		cout << "&";
+		getline(cin, input);
+		cout << input.substr(0, input.find(" ")) << endl;
+		commands[input.substr(0, input.find(" "))](input.substr(input.find(" ")));
+	}
+}
+
+
