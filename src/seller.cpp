e44d6b851a2bc223ec1e41c743b9ed8befe20786
svinthefirst <kerineba@gmail.com>
2025-10-29 16:21:52 +0200

    feat: нерабочая частичная реализация функций продавца

--- src/seller.cpp
+++ src/seller.cpp
@@ -2,32 +2,40 @@
 #include <iostream>
 #include "../include/storage.h"
 #include <map>
+#include <vector>
 
 using namespace std;
 
 bool served = false;
+vector<product> products;
 
-void sell_products(string args)
+void sell_products(vector<string> args)
 {
-	cout << "args: " << args << endl;
+	//cout << "args: " << args << endl;
 }
 
-void add_product(string args)
+void add_product(vector<string> args)
 {
-	cout << "args: " << args << endl;
+	string a = "35435";
+	cout << stoi(a, nullptr, 10);
+	//cout << "args: " << args << endl;
+	cout << "name, price, quanity" << endl;
+	add_item(stoi(args[0], nullptr, 10), (unsigned)args[1], (unsigned)args[2]);
 }
 
-void show_products(string args)
+void show_products(vector<string> args)
 {
-	cout << "args: " << args << endl;
+	//cout << "args: " << args << endl;
+	show_storage();
 }
 
-void ring_up_product(string args)
+void ring_up_product(vector<string> args)
 {
-	cout << "args: " << args << endl;
+	//cout << "args: " << args << endl;
+	products
 }
 
-map<string, void(*)(string)> commands { {"add", add_product}, { "sell", sell_products }, { "show", show_products }, { "ring", ring_up_product } };
+map<string, void(*)(string[])> commands{{"add", add_product}, {"sell", sell_products}, {"show", show_products}, {"ring", ring_up_product}};
 
 void serve_buyer(string product_name, int quantity)
 {
@@ -39,7 +47,8 @@ void serve_buyer(string product_name, int quantity)
 		cout << input.substr(0, input.find(" ")) << endl;
 
 		string func_name = input.substr(0, input.find(" "));
-		string args = input.find(" ") != string::npos ? input.substr(input.find(" ") - 1) : "";
+		string argsLine = input.find(" ") != string::npos ? input.substr(input.find(" ") - 1) : "";
+		vector<string> args = argsLine.Split(" ");
 		if (commands.find(func_name) != commands.end())
 			commands[func_name](args);
 	}
